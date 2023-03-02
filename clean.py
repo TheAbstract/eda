53e7910214038d856e50f43cf41bd1eed0993a1d
siphiwe <siphiwe.m@protonmail.com>
2023-03-02 13:23:34 +0200

    feat: add `clean.py` for common tasks before looking into data.

---
+++ clean.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def is_empty(df, plot=False):
+    null = df.isnull()
+    if plot:
+        sns.heatmap(null.T, xticklabels=False, cbar=False, cmap='binary')
+        plt.show()
+    else:
+        return null.mean().sort_values()[::-1]
+
+
+def drop_empty(df, percent=0.8):
+    thresh = len(df) * (1 - percent)
+    return df.dropna(thresh=thresh, axis=1)
