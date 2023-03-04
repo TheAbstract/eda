2279d63ad30a5ca15aeb11e9b526411f47d8769f
siphiwe <siphiwe.m@protonmail.com>
2023-03-04 15:34:18 +0200

    feat: fill missing values, objects with mode, numbers with median.

--- clean.py
+++ clean.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def is_empty(df, plot=False):
+def is_missing(df, plot=False):
     null = df.isnull()
     if plot:
         sns.heatmap(null.T, xticklabels=False, cbar=False, cmap='binary')
@@ -11,12 +11,19 @@ def is_empty(df, plot=False):
     else:
         return null.mean().sort_values()[::-1]
 
-def drop_empty(df, percent=0.8):
+def drop_missing(df, percent=0.8):
     thresh = len(df) * (1 - percent)
     return df.dropna(thresh=thresh, axis=1)
+
+def fill_missing(df):
+    for col in df:
+        if df[col].dtypes == 'object':
+            mode = df[col].value_counts().index[0]
+            df[col].fillna(mode, inplace=True)
+        if pd.api.types.is_numeric_dtype(df[col]):
+            df[col].fillna(df[col].median(), inplace=True)
 
 def numeric_split(df):
     numeric = df.select_dtypes('number')
     objects = df.select_dtypes('object')
     return numeric, objects
-
