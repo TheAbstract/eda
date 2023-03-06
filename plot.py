843c01e3ce024254b07aee67c5b762523d7973fa
siphiwe <siphiwe.m@protonmail.com>
2023-03-06 20:58:50 +0200

    feat: add data visualisation module. histbox idea from kaggle user @janmpia.

---
+++ plot.py
@@ -0,0 +1,13 @@
+def histbox(df, column):
+    '''
+    plots a histogram with a box-and-whiskers at the top.
+    got this wonderful idea from @janmpia
+        source - https://www.kaggle.com/competitions/playground-series-s3e8/discussion/391832
+    '''
+    f, (ax_box, ax_hist) = plt.subplots(2, sharex=True, gridspec_kw={'height_ratios': (.15, .85)})
+    ax_box.title.set_text(f'{column} histogram and boxplot')
+    sns.boxplot(df['column'], orient='h', ax=ax_box)
+    sns.histplot(data=df, x=column, ax=ax_hist)
+    # remove x axis name for the boxplot
+    ax_box.set(xlabel='')
+    plt.show()
