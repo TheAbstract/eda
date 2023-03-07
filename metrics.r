c1d59237258ff3c3b5525e6293023d9fd3300046
siphiwe <siphiwe.m@protonmail.com>
2023-03-07 22:00:17 +0200

    refactor: don’t need separate file for settings.

--- metrics.r
+++ metrics.r
@@ -4,7 +4,7 @@ auc <- function(outcomes, predictions) {
 }
 
 'Given the outcomes and predictions of a model, uses the ROCR library to plot the ROC.'
-plot_roc <- function(outcomes, predictions){
+plot_roc <- function(outcomes, predictions) {
   ROC <- performance(prediction(predictions, outcomes), 'tpr', 'fpr')
   plot(ROC, colorize=TRUE, print.cutoffs.at=seq(0, 1, 0.1), text.adj=c(-0.2, 1.7))
 }
