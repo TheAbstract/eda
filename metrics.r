c675c404cdea0dafe2665d71572b2f0dbd35f981
siphiwe <siphiwe.m@protonmail.com>
2023-03-15 06:43:49 +0200

    feat: add `accuracy` function.

--- metrics.r
+++ metrics.r
@@ -5,6 +5,16 @@ auc <- function(outcomes, predictions) {
 
 'Given the outcomes and predictions of a model, uses the ROCR library to plot the ROC.'
 plot_roc <- function(outcomes, predictions) {
-  ROC <- performance(prediction(predictions, outcomes), 'tpr', 'fpr')
+  ROC = performance(prediction(predictions, outcomes), 'tpr', 'fpr')
   plot(ROC, colorize=TRUE, print.cutoffs.at=seq(0, 1, 0.1), text.adj=c(-0.2, 1.7))
+}
+
+'Takes outcomes and predictions from a classifier. 
+ Computes the confussion matrix and outputs accuracy, TPR, and TNR.'
+accuracy <- function(outcomes, predictions) {
+  CM = table(outcomes, predictions)
+  TotalAccuracy = (CM[1, 1] + CM[2, 2]) / (CM[2, 1] + CM[2, 2] + CM[1, 1] + CM[1, 2])
+  TPR = CM[2, 2] / (CM[2, 1] + CM[2, 2])
+  TNR = CM[1, 1] / (CM[1, 1] + CM[1, 2])
+  as.data.frame(col.names=c('TotalAccuracy', 'TPR', 'TNR'), list(TotalAccuracy, TPR, TNR))
 }
